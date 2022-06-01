0d65e418e24740b593057a8f38a1dfd6261a54c3
deloluv <75827880+deloluv@users.noreply.github.com>
2022-06-01 02:39:32 -0400

    cleaned typing on team cards

--- static/js/builder.js
+++ static/js/builder.js
@@ -22,40 +22,54 @@ const DATASTORE = {
     }
 };
 
+// COMPLETE
 async function checkDataStore(type, data) {
     if (type == 'pokemon') {
         const KEY = data['name'].toLowerCase();
         if (DATASTORE['pokemon'].hasOwnProperty(KEY)) {
+            // Search for Name
             if (DATASTORE['pokemon'][KEY].hasOwnProperty('types')) {
+                // Return DataStore Entry
                 return DATASTORE['pokemon'][KEY]['types'];
             } else {
+                // Fetching Pokemon Entry
                 await d3.json(`https://pokeapi.co/api/v2/pokemon/${KEY}`)
                     .then(res => {
                         let types = res['types'];
+                        // Save to DataStore
                         DATASTORE['pokemon'][KEY]['types'] = types;
                         for (let i = 0; i < types.length; i++) {
+                            // Update DataStore
                             checkDataStore('types', types[i]['type']['name']);
                         };
+                        // Return Completed DataStore Entry
                         return DATASTORE['pokemon'][KEY]['types'];
                     });
             };
         } else {
+            // Add Name to DataStore
             DATASTORE['pokemon'][data['name'].toLowerCase()] = data;
+            // Update DataStore
             return checkDataStore('pokemon', data);
         }
     } else if (type == 'types') {
         if (DATASTORE['types'][data].hasOwnProperty('damage_relations')) {
+            // Returns DataStore Entry
             return DATASTORE['types'][data]['damage_relations']
         } else {
+            // Fetches Type Entry
             await d3.json(`https://pokeapi.co/api/v2/type/${data}`)
                 .then(res => {
+                    // Update DataStore
                     DATASTORE['types'][data]['damage_relations'] = res['damage_relations'];
+                    // Returns DataStore Entry
                     return DATASTORE['types'][data]['damage_relations'];
                 });
         };
     }
 };
 
+// COMPLETE
 function checkAvailability() {
     const SPOT = d3.select('#poke_team').select('.team-unused');
     if (SPOT.empty()){
@@ -64,12 +78,15 @@ function checkAvailability() {
     } else { return true; };
 };
 
+// IN PROGRESS
 function calculateEffectiveness(type_list) {
 
     return [];
 };
 
+// IN PROGRESS
 function addToTeam(name) {
+    // Selections
     const TEAM_BUILD = d3.select('#poke_team');
     const TEAM_SPOT = TEAM_BUILD.select('.team-unused')
         .classed('team-unused', false)
@@ -80,50 +97,75 @@ function addToTeam(name) {
     const TEAM_NAME = TEAM_SPOT.select('#team-name');
     const TEAM_SUPEREFF = TEAM_SPOT.select('#supereff-team');
     const TEAM_NOTEFF = TEAM_SPOT.select('#noteff-team');
-
+    // Data
     const POKEMON = DATASTORE['pokemon'][name.toLowerCase()];
     const POKE_SPRITE = POKEMON['img'];
     const POKE_TYPE = POKEMON['types'];
     const POKE_NAME = POKEMON['name'];
-
+    
+    // Name
+    TEAM_NAME.append('span')
+        .classed('text-center', true)
+        .classed('fw-bold', true)
+        .text(POKE_NAME);
+    // Sprite
     TEAM_SPRITE.attr('src', POKE_SPRITE);
+    // Type
+    const TEAM_TYPE_SPOT = TEAM_TYPE.append('div')
+        .classed('row', true)
+        .classed('p-2', true)
+        .classed('gap-2', true);
     for (let i = 0; i < POKE_TYPE.length; i++) {
         let curType = POKE_TYPE[i]['type']['name'];
-        let typeProper = curType.charAt(0).toUpperCase() + curType.slice(1);
         let typeHex = DATASTORE['types'][curType]['color'];
-        TEAM_TYPE.append('div')
-            .classed('container', true)
+        TEAM_TYPE_SPOT.append('div')
+            .classed('col', true)
             .classed('rounded', true)
-            .style('background-color', typeHex)
-            .append('p')
             .classed('text-center', true)
-            .text(typeProper);
+            .classed('border', true)
+            .classed('box-shadow-effect', true)
+            .style('background-color', typeHex)
+            .append('span')
+            .classed('text-light', true)
+            .classed('text-uppercase', true)
+            .classed('fw-bold', true)
+            .text(curType);
     };
-    TEAM_NAME.append('p')
-        .classed('text-center', true)
-        .text(POKE_NAME);
+    // Super Effective
+    // Not Effective
 };
 
+// IN PROGRESS
 function removeFromTeam(pokemon) {
     const TEAM_BUILD = d3.select('#poke_team');
+    // Used Class Tag
     const SPOT = TEAM_BUILD.select(`#${pokemon}`)
         .classed('team-unused', true)
         .classed('team-used', false)
         .html("");
+    // Reset Name
+    SPOT.append('div')
+        .attr('id', 'team-name')
+        .classed('text-center', true); 
+    // Reset Img
     SPOT.append('img')
         .attr('id', 'team-sprite')
         .classed('mx-auto', true)
         .classed('d-block', true)
         .attr('src', '/static/imgs/pokeball.png');
+    // Reset Type
     SPOT.append('div')
-        .attr('id', 'team-type');
-    SPOT.append('div')
-        .attr('id', 'team-name');    
+        .classed('container', true)
+        .attr('id', 'team-type'); 
+    // Reset Super Effective
+    // Reset Not Effective  
 };
 
+// IN PROGRESS
 function removeAll() {
     const TEAM_BUILD = d3.select('#poke_team').html("");
     for (let i = 0; i < 6; i++) {
+        // Reset Card
         const SPOT = TEAM_BUILD.append('div')
             .classed('team-unused', true)
             .classed('col-6', true)
@@ -132,33 +174,46 @@ function removeAll() {
             .classed('col-lg-2', true)
             .classed('box-shadow-effect', true)
             .classed('p-3', true);
+        // Reset Name
+        SPOT.append('div')
+            .attr('id', 'team-name')
+            .classed('text-center', true);
+        // Reset Img
         SPOT.append('img')
             .attr('id', 'team-sprite')
             .classed('mx-auto', true)
             .classed('d-block', true)
             .attr('src', '/static/imgs/pokeball.png');
+        // Reset Type
         SPOT.append('div')
-            .attr('id', 'team-type');
-        SPOT.append('div')
-            .attr('id', 'team-name');  
+            .classed('container', true)
+            .attr('id', 'team-type'); 
+        // Reset Pokedex Selections 
         d3.selectAll('.selected')
             .style('background-color', 'white')
             .classed('selected', false);
     };
-}
+};
 
+// COMPLETE
 async function onSelect(pokemon, elementID) {
-    console.log(DATASTORE);
     const CUR_POKEMON = d3.select(`#${elementID}`);
     if (CUR_POKEMON.classed('selected')) {
+        // Deselecting
         CUR_POKEMON.style('background-color', 'white')
+            .style('border-color', 'white')
             .classed('selected', false);
         removeFromTeam(pokemon['name'])
         return
     }
+    // Check Space on Team
     if (!checkAvailability()) { return };
+    // Select
     CUR_POKEMON.style('background-color', 'yellowgreen')
+        .style('border-color', '#8bb92d')
         .classed('selected', true);
+    // Upload to DataStore
     await checkDataStore('pokemon', pokemon);
+    // Update Team
     await addToTeam(pokemon['name']);
 };
